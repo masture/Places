0256cdb5b348f208c545a7115146b94cb912191d
Pankaj Kulkarni <pankajvkulkarni@icloud.com>
2018-06-18 14:17:52 +0530

    Corrected Indentation

--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -26,88 +26,88 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var mapView: MKMapView!
-  
-  fileprivate let locationManager = CLLocationManager()
-  fileprivate var startedLoadingPOIs = false
-  fileprivate var places = [Place]()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-    locationManager.startUpdatingLocation()
-    locationManager.requestWhenInUseAuthorization()
+    @IBOutlet weak var mapView: MKMapView!
     
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  @IBAction func showARController(_ sender: Any) {
-  }
-  
+    fileprivate let locationManager = CLLocationManager()
+    fileprivate var startedLoadingPOIs = false
+    fileprivate var places = [Place]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showARController(_ sender: Any) {
+    }
+    
 }
 
 // MARK - CLLocationManagerDelegate Extenssion
 extension ViewController : CLLocationManagerDelegate {
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    
-    if locations.count > 0 {
-      
-  
-      
-      
-      let location = locations.last!
-      print("Accuracy: \(location.horizontalAccuracy)")
-      
-      if location.horizontalAccuracy < 100 {
-        manager.stopUpdatingLocation()
-        let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
-        let region = MKCoordinateRegion(center: location.coordinate, span: span)
-        mapView.region = region
-      }
-      
-      if !startedLoadingPOIs {
-        startedLoadingPOIs = true
-        let loader = PlacesLoader()
-        loader.loadPOIS(location: location, radius: 1000) { (placeDict, error) in
-          
-          if let dict = placeDict {
-
-            guard let placesArray = dict.object(forKey: "results") as? [NSDictionary]  else { return }
-
-            for placeDict in placesArray {
-              let latitude = placeDict.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
-              let longitude = placeDict.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
-              let reference = placeDict.value(forKey: "reference") as! String
-              let name = placeDict.value(forKey: "name") as! String
-              let address = placeDict.value(forKey: "vicinity") as! String
-              
-              let location = CLLocation(latitude: latitude, longitude: longitude)
-              
-              let place = Place(location: location, reference: reference, name: name, address: address)
-              self.places.append(place)
-              
-              let annotation = PlaceAnnotation(location: location.coordinate, title: place.placeName)
-              
-              DispatchQueue.main.async {
-                self.mapView.addAnnotation(annotation)
-              }
-              
-              
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            
+            
+            
+            
+            let location = locations.last!
+            print("Accuracy: \(location.horizontalAccuracy)")
+            
+            if location.horizontalAccuracy < 100 {
+                manager.stopUpdatingLocation()
+                let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
+                let region = MKCoordinateRegion(center: location.coordinate, span: span)
+                mapView.region = region
             }
             
-          }
-          
+            if !startedLoadingPOIs {
+                startedLoadingPOIs = true
+                let loader = PlacesLoader()
+                loader.loadPOIS(location: location, radius: 1000) { (placeDict, error) in
+                    
+                    if let dict = placeDict {
+                        
+                        guard let placesArray = dict.object(forKey: "results") as? [NSDictionary]  else { return }
+                        
+                        for placeDict in placesArray {
+                            let latitude = placeDict.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
+                            let longitude = placeDict.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
+                            let reference = placeDict.value(forKey: "reference") as! String
+                            let name = placeDict.value(forKey: "name") as! String
+                            let address = placeDict.value(forKey: "vicinity") as! String
+                            
+                            let location = CLLocation(latitude: latitude, longitude: longitude)
+                            
+                            let place = Place(location: location, reference: reference, name: name, address: address)
+                            self.places.append(place)
+                            
+                            let annotation = PlaceAnnotation(location: location.coordinate, title: place.placeName)
+                            
+                            DispatchQueue.main.async {
+                                self.mapView.addAnnotation(annotation)
+                            }
+                            
+                            
+                        }
+                        
+                    }
+                    
+                }
+            }
+            
         }
-      }
-      
     }
-  }
 }
